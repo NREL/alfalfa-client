89c13ccc46d607af84f3187657638f09711a3667
corymosiman12 <cory.mosiman@nrel.gov>
2020-01-07 19:07:40 -0700

    merge branches locally

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='alfalfaclient',
+      version='0.1dev',
+      description='A standalone client for the NREL Alfalfa application',
+      url='https://github.com/corymosiman12/alfalfaclient',
+      author='Cory Mosiman',
+      license='MIT',
+      packages=['alfalfaclient'],
+      long_description=open('README.MD').read())
