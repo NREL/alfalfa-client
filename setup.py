0a5429b0e0f42a5f88069a0f62c7a55f091b9fd9
Nicholas Long <nicholas.long@nrel.gov>
2020-08-03 22:22:04 -0600

    rename to alfalfa_client

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-setup(name='alfalfa-client',
-      version='0.1.dev2',
+setup(name='alfalfa_client',
+      version='0.1.pr1',
       description='A standalone client for the NREL Alfalfa application',
       url='https://github.com/nrel/alfalfa-client',
       author=open('AUTHORS.md'),
-      license='MIT',
-      packages=['alfalfa-client'],
+      license='BSD',
+      packages=['alfalfa_client'],
       long_description=open('README.MD').read(),
       install_requires=[
-            'numpy',
-            'pandas',
-            'requests',
-            'requests-toolbelt'
+            'numpy==1.18.1',
+            'pandas==0.25.3',
+            'requests==2.24.0',
+            'requests-toolbelt==0.9.1'
       ])
