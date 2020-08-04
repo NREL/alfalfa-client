cac896b4ce2be9431d5452f70187060f476d2c4a
Nicholas Long <nicholas.long@nrel.gov>
2020-08-03 22:43:14 -0600

    historian docstring

--- alfalfa_client/historian.py
+++ alfalfa_client/historian.py
@@ -9,11 +9,14 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 class Historian(object):
     def __init__(self, time_step):
+        """
+        :param time_step: int, time step in minutes
+        """
         self.data = {}
         self.name_map = {}
         self.units = {}
         self.conversion_map = {}
-        self.time_step = time_step # time_step stored in minutes
+        self.time_step = time_step
 
     def add_point(self, name, units, point_name, f_conversion=None):
         """
