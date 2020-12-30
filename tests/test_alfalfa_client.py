ab138f8a7e6cefe61cd5c1731f4eba468946c4eb
corymosiman12 <cory.mosiman12@gmail.com>
2020-12-29 23:31:13 -0500

    fix tests

--- tests/test_alfalfa_client.py
+++ tests/test_alfalfa_client.py
@@ -4,11 +4,11 @@ import alfalfa_client.historian as ah
 
 class TestAlfalfaClient:
     def test_instantiation(self):
-        client = ac.AlfalfaClient
+        client = ac.AlfalfaClient()
         assert client is not None
 
 
 class TestHistorian:
     def test_historian_instantiation(self):
-        hist = ah.Historian
+        hist = ah.Historian()
         assert hist is not None
