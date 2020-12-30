c4fe69dbdf1f8afd96261c60d009dbca84c324e4
corymosiman12 <cory.mosiman12@gmail.com>
2020-12-30 15:14:34 -0500

    update tests

--- tests/test_alfalfa_client.py
+++ tests/test_alfalfa_client.py
@@ -13,13 +13,14 @@ class TestAlfalfaClient:
         assert client.url == 'http://localhost'
 
     def test_construct_point_write_grid(self):
-        point_id = hszinc.Ref('bfc28235-826e-4af0-8645-f93bd53cbb3b')
+        string_id = 'bfc28235-826e-4af0-8645-f93bd53cbb3b'
+        point_id = hszinc.Ref(string_id)
         value = hszinc.Quantity(2)
         level = hszinc.Quantity(1)
         who = 'me'
         g = ac.AlfalfaClient.construct_point_write_grid(point_id, value, level, who)
         expected = {
-            'meta': {'ver': '3.0'},
+            'meta': {'ver': '2.0'},
             'cols': [
                 {'name': 'id'},
                 {'name': 'value'},
@@ -27,10 +28,10 @@ class TestAlfalfaClient:
                 {'name': 'who'},
             ],
             'rows': [{
-                'id': f"r:{point_id}",
+                'id': f"r:{string_id}",
                 'value': f"n:2.000000",
                 'level': f"n:1.000000",
-                'who': f"s:{who}"
+                'who': f"s:me"
             }]
         }
         assert hszinc.dump(g, hszinc.MODE_JSON) == json.dumps(expected)
