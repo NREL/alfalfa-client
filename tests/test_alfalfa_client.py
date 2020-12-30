0b11beb3225ac8feb676cfc229db0c97322637ed
corymosiman12 <cory.mosiman12@gmail.com>
2020-12-30 11:46:52 -0500

    update tests

--- tests/test_alfalfa_client.py
+++ tests/test_alfalfa_client.py
@@ -1,3 +1,7 @@
+import json
+
+import hszinc
+
 import alfalfa_client.alfalfa_client as ac
 import alfalfa_client.historian as ah
 
@@ -7,6 +11,29 @@ class TestAlfalfaClient:
         client = ac.AlfalfaClient()
         assert client is not None
         assert client.url == 'http://localhost'
+
+    def test_construct_point_write_grid(self):
+        id = 'bfc28235-826e-4af0-8645-f93bd53cbb3b'
+        value = 2
+        level = 1
+        who = 'me'
+        g = ac.AlfalfaClient.construct_point_write_grid(id, value, level, who)
+        expected = {
+            'meta': {'ver': '3.0'},
+            'cols': [
+                {'name': 'id'},
+                {'name': 'value'},
+                {'name': 'level'},
+                {'name': 'who'},
+            ],
+            'rows': [{
+                'id': f"r:{id}",
+                'value': f"n:2.000000",
+                'level': f"n:1.000000",
+                'who': f"s:{who}"
+            }]
+        }
+        assert hszinc.dump(g, hszinc.MODE_JSON) == json.dumps(expected)
 
 
 class TestHistorian:
